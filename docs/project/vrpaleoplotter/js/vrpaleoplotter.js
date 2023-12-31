e594d50bfe64a0d5361bc3c5ee01178a2cc626bf
The Scaffold of Witan <32809826+ScaffoldOfWitan@users.noreply.github.com>
2023-12-31 20:50:33 +0100

    aaaaa

--- docs/project/vrpaleoplotter/js/vrpaleoplotter.js
+++ docs/project/vrpaleoplotter/js/vrpaleoplotter.js
@@ -841,7 +841,7 @@ function shadeConvexSet() {
         //leftX = rValue.Rx - fraction;
         rightX = rValue.Lx + fraction;
     }
-    width=rValue.Rpix - leftX - (rValue.Rpix.rightX);
+    width=rValue.Rpix - leftX - (rValue.Rpix-rightX);
     width = rValue.reverseR?width:(-width);
     height = rValue.By;
     console.info("ctx.fillRect(" + leftX.floor + "," + rValue.Ty + "," + width.floor + "," + height + ")");        
