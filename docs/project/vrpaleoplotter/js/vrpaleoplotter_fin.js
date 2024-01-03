278481cf0e8e5a8edc12d3a2391648ae047d2eed
The Scaffold of Witan <32809826+ScaffoldOfWitan@users.noreply.github.com>
2024-01-03 19:19:41 +0100

    All functions not currently in focus to vrpaleoplotter_fin.js

--- docs/project/vrpaleoplotter/js/vrpaleoplotter_fin.js
+++ docs/project/vrpaleoplotter/js/vrpaleoplotter_fin.js
@@ -534,3 +534,256 @@ function setEventListeners() {
     }
 
 }
+
+/**
+ * @abstract 
+ */
+function initGraphBox() {
+    // var graphBox
+    var gBox = {
+        graphId: "",
+        graphCalib: "", //  Iest data
+        top: {
+            x: 103,     //  78      103,
+            y: 15       //  85      15
+        },
+        bottom: {
+            x: 393,     //  583     393
+            y: 420      //  310     420
+        },
+        rPix: "<calc>",
+        rYears: 20000,
+        yPerPix: "<calc>",
+        reverseX: true,
+        gFileName: "",
+        gStored: "",
+        dsetURI: "",
+        gURI: "",
+        gDocURI: ""
+    }
+
+    /**
+     * include the fetch 
+     * 
+     */
+    let gid = '0'
+    let url = ''
+    let id = '';
+
+    // Fetch 'tbl_gBox'
+    id = '1bB-SIFalx3B3WfEzbuobIr6utT0kU7LKuEyuxzLT2VY';
+    //arrayFromSheet(tbl_gBox_id);
+    url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?tqx=out:json&tq&gid=' + gid;
+    fetch(url)
+        .then(response => response.text())
+        .then(data => document.getElementById("place_gbox").innerHTML = sheetToTbl(data.substring(47).slice(0, -2), "tbl_gbox", "table table-striped caption-top table-sm table-hover", "tbl gbox"))
+
+
+    return gBox
+}
+
+function initArrayLstTables() {
+
+    let lstTables = {
+        table: "",
+        info: "",
+        sheetId: "",
+        location: "",
+        dataWikiURI: "",
+        DiscordSrvr: "",
+        dataDocURI: ""
+    }
+
+    let gid = '0'
+    let url = ''
+    let id = '';
+    // Fetch 'data_vrpaleoplotter';
+    // For delay add .then(sleeper(20)) in fetch().then list
+    id = '1p6pFIaJQI_KIA8KFmJQmAkF8b9HzX1y6NCu9gIBTaDg';
+    url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?tqx=out:json&tq&gid=' + gid;
+    fetch(url)
+        .then(response => response.text())
+        .then(data => document.getElementById("place_tbl_tbllist").innerHTML = sheetToTbl(data.substring(47).slice(0, -2), "tbl_tbllist", "table table-striped caption-top table-sm table-hover", "tbl_tbllist"))
+
+    return lstTables
+}
+/**
+ * @abstract The m(arker) Plot array: mPlot array. The array contains the active mPlot session. Each mPlot session: plotId; Description. A focus list of Royal cubits for this plot. Links to: plotWikiURI and a plotDocURI. 
+ * TODO: getSheetData() imports google sheet data to HTML tabels. Replace the use of the arrays by use of the HTML tabels.
+ */
+function initArrayMplot() {
+    let mPlot = {
+        plotId: "",
+        Description: "",
+        graphlist: ['string 1', 'string 2', 'string 3'],
+        plotWikiURI: "",
+        DiscordSrvr: "",
+        plotDocURI: "",
+        mRoyalCubits: ""
+    }
+    /**
+     * @abstract Populate mPlot array (todo: UI, db). Db of plots and callibration info.
+     * TODO: getSheetData() imports google sheet data to HTML tabels. Replace the use of the arrays by use of the HTML tabels.
+     */
+    mPlot.plotId = "vr_plt_0000001";
+    mPlot.Description = "Comparitive mappins";
+    //mplot.graphlist = eval('['+'grph00000000,grph00000001'+']');
+    mPlot.plotWikiURI = "void";
+    mPlot.DiscordSrvr = "void";
+    mPlot.plotDocURI = "void";
+    mPlot.mRoyalCubits = "";
+    /**
+     * include the fetch 
+     * 
+     */
+    let gid = '0'
+    let url = ''
+    let id = '';
+    // Fetch 'tbl_mplot'
+    id = '1QRR_9DZsyRXTc2hsp4U5YX8gC4vtbWCi2KFugu63Cug';
+    //arrayFromSheet(tbl_mplot_id);
+    url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?tqx=out:json&tq&gid=' + gid;
+    fetch(url)
+        .then(response => response.text())
+        .then(data => document.getElementById("place_mplot").innerHTML = sheetToTbl(data.substring(47).slice(0, -2), "tbl_mplot", "table table-striped caption-top table-sm table-hover", "tbl mplot"))
+
+
+    return mPlot
+}
+
+/**
+* @abstract Status array sets. Each set has an arithmetic template for expanding the core seed values into mappings of events in time. Template string literals in back-tic:'a*b*c*...', `${a}*${b}*${c}*...`, execute automatically. Therefore are stored in quotes "2*2.02*2.52* ..", triggered --> calc = eval("2*2.02*2.52* .."). Depending on AorM={A,M}, zero value columns get set to 0 or 1, to preserve overall results. A(dditive)-> 0, or M(ultiplicative), e.g., (A)[a+0+c+d+0+...], or (M)[a*1*c*d*1*...]
+* TODO: getSheetData() imports google sheet data to HTML tabels. Replace the use of the arrays by use of the HTML tabels.
+*/
+function initArraySets() {
+    let lstSet = {
+        set1: {
+            chkId: 'chk_s1',
+            show: false,
+            info: "Set 1",
+            strlit: 'a*b*c*d*e*f*g*h*i',
+            AorM: 'M',
+            csvRc: ""
+        },
+        set2: {
+            chkId: 'chk_s2',
+            show: false,
+            info: "Set 2",
+            strlit: 'a+b+c+d+e+f+g+h+i',
+            AorM: 'A',
+            csvRc: ""
+        },
+        set3: {
+            chkId: 'chk_s3',
+            show: false,
+            info: "Set 3",
+            strlit: 'Math.sqrt(a+b+c+d+e+f+g+h+i)',
+            AorM: 'A',
+            csvRc: ""
+        },
+        set4: {
+            chkId: 'chk_s4',
+            show: false,
+            info: "Set 4",
+            strlit: 'Math.sqrt(a*b*c*d*e*f*g*h*i)',
+            AorM: 'M',
+            csvRc: ""
+        },
+        set5: {
+            chkId: 'chk_s5',
+            show: false,
+            info: "Set 5",
+            strlit: '(a**2)*(b**2)*(c**2)*(d**2)*(e**2)*(f**2)*(g**2)*(h**2)*(i**2)',
+            AorM: 'M',
+            csvRc: ""
+        },
+        set6: {
+            chkId: 'chk_s6',
+            show: false,
+            info: "Set 6",
+            strlit: '(a**0.5)+(b**0.5)+(c**0.5)+(d**0.5)+(e**0.5)+(f**0.5)+(g**0.5)+(h**0.5)+(i**0.5)',
+            AorM: 'A',
+            csvRc: ""
+        },
+        addSets: {
+            chkId: null,
+            show: false,
+            info: "Cumulative list of sets",
+            strlit: null,
+            AorM: null,
+            csvRc: ""
+        },
+    }
+    /**
+     * include the fetch 
+     * 
+     */
+    let gid = '0'
+    let url = ''
+    let id = '';
+
+    // Fetch 'tbl_sets'
+    id = '1jKLwi9l8ck5KgbgmhwG4clmd70b6zKvsPEUPA036Tlk';
+    //arrayFromSheet(tbl_sets_id);
+    url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?tqx=out:json&tq&gid=' + gid;
+    fetch(url)
+        .then(response => response.text())
+        .then(data => document.getElementById("place_sets").innerHTML = sheetToTbl(data.substring(47).slice(0, -2), "tbl_sets", "table table-striped caption-top table-sm table-hover", "tbl sets"))
+
+    return lstSet
+}
+/**
+ * @abstract Status array presets
+ * TODO: getSheetData() imports google sheet data to HTML tabels. Replace the use of the arrays by use of the HTML tabels.
+ */
+function initArrayPresets() {
+    let lstPreset = {
+        f1: {
+            chkId: 'chk_f1',
+            show: false,
+            info: "The mapping for 1 Royal cubit:f(1))",
+            csvRc: "1"
+        },
+        seeds: {
+            chkId: 'chk_seeds',
+            show: false,
+            info: "First passage; 5 measures",
+            csvRc: "2,2.02,2.52,2.98,5.5"
+        },
+        kc: {
+            chkId: 'chk_kc',
+            show: false,
+            info: "Height of the King's chamber",
+            csvRc: "11.17"
+        },
+        extr: {
+            chkId: 'chk_extr',
+            show: false,
+            info: "Left, and right infinity convex set",
+            csvRc: "19.099999,999999999"
+        },
+        addPresets: {
+            chkId: null,
+            show: false,
+            info: "Cumulative list of presets",
+            csvRc: ""
+        }
+    }
+    /**
+     * include the fetch 
+     * 
+     */
+    let gid = '0'
+    let url = ''
+    let id = '';
+    // Fetch 'tbl_presets'
+    id = '1GdTl9vU4KPBG9nA9g6XWcVX2__PuAh1Y5HkrogYLhyw';
+    //arrayFromSheet(tbl_presets_id);
+    url = 'https://docs.google.com/spreadsheets/d/' + id + '/gviz/tq?tqx=out:json&tq&gid=' + gid;
+    fetch(url)
+        .then(response => response.text())
+        .then(data => document.getElementById("place_presets").innerHTML = sheetToTbl(data.substring(47).slice(0, -2), "tbl_presets", "table table-striped caption-top table-sm table-hover", "tbl presets"))
+
+
+    return lstPreset
+}
