32d2d1d8a4268c22545acbb9d337ee7b69a68f32
The Scaffold of Witan <32809826+ScaffoldOfWitan@users.noreply.github.com>
2024-01-10 15:17:03 +0100

    Preset lables shortened for better mobile render

--- docs/project/vrpaleoplotter/subdocs/fieldset_selectpresets_div.js
+++ docs/project/vrpaleoplotter/subdocs/fieldset_selectpresets_div.js
@@ -2,23 +2,17 @@ const fieldset_selectpresets_div = document.querySelector('#fieldset_selectprese
 fieldset_selectpresets_div.insertAdjacentHTML('beforeend', `
 <!-- start preset fieldset -->
 <fieldset>
-    <legend>Select presets:</legend>
-    <xxx_form class="frm_changepreset" id="frm_changepreset">
-        <input id="chk_extr" type="checkbox" name="presets" value="19.0999,999999" /><label
-            for="chk_extr">Map
-            extremes convex set</label><br />
+<legend>Select presets:</legend>
+    <input id="chk_extr" type="checkbox" name="presets" value="19.0999,999999" /><label
+        for="chk_extr">Bounds convex set</label><br />
 
-        <input id="chk_f1" type="checkbox" name="presets" value="1" /><label for="chk_f1">Map
-            f(1)
-            Unit value</label><br />
+    <input id="chk_f1" type="checkbox" name="presets" value="1" /><label for="chk_f1">f(1)
+        Unit value</label><br />
 
-        <input id="chk_kc" type="checkbox" name="presets" value="11.17" /><label for="chk_kc">Map
-            f(11.17) Hight Kings Chamber</label><br />
+    <input id="chk_kc" type="checkbox" name="presets" value="11.17" /><label for="chk_kc">f(11.17) Hight Kc</label><br />
 
-        <input id="chk_seeds" type="checkbox" name="presets" value="2,2.02,2.52,2.98,5.5" /><label
-            for="chk_seeds">The 1<sub>th</sub> Passage
-            (reset the 5 seeds)</label>
-    </xxx_form>
+    <input id="chk_seeds" type="checkbox" name="presets" value="2,2.02,2.52,2.98,5.5" /><label
+        for="chk_seeds">The 1<sub>th</sub> Passage</label>
 </fieldset>
 <!-- end preset fieldset -->
 `);
