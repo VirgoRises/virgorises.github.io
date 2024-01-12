5a12bbffe57e2dce2d164665c156f6a70018c17b
The Scaffold of Witan <32809826+ScaffoldOfWitan@users.noreply.github.com>
2024-01-12 11:01:31 +0100

    Optimized responsive: shortened text, rearranged input elements

--- docs/project/vrpaleoplotter/subdocs/fieldset_selectpresets_div.js
+++ docs/project/vrpaleoplotter/subdocs/fieldset_selectpresets_div.js
@@ -1,17 +1,27 @@
-const fieldset_selectpresets_div = document.querySelector('#fieldset_selectpresets_div');                          
+const fieldset_selectpresets_div = document.querySelector('#fieldset_selectpresets_div');
 fieldset_selectpresets_div.insertAdjacentHTML('beforeend', `
 <!-- start preset fieldset -->
 <fieldset>
-    <input id="chk_extr" type="checkbox" name="presets" value="19.0999,999999" /><label
-        for="chk_extr">Bounds convex set</label><br />
-
-    <input id="chk_f1" type="checkbox" name="presets" value="1" /><label for="chk_f1">f(1)
-        Unit value</label><br />
-
-    <input id="chk_kc" type="checkbox" name="presets" value="11.17" /><label for="chk_kc">f(11.17) Hight Kc</label><br />
-
-    <input id="chk_seeds" type="checkbox" name="presets" value="2,2.02,2.52,2.98,5.5" /><label
-        for="chk_seeds">The 1<sub>th</sub> Passage</label>
+    <div class="row">
+        <div class="col">
+            <input id="chk_extr" type="checkbox" name="presets" value="19.0999,999999" />
+            <label for="chk_extr"><small>Convex set</small></label>
+        </div>  
+        <div class="col">
+            <input id="chk_f1" type="checkbox" name="presets" value="1" />
+            <label for="chk_f1"><small>f(1) Unit value</small></label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <input id="chk_kc" type="checkbox" name="presets" value="11.17" />
+            <label for="chk_kc"><small>f(11.17) Height Kc</small></label>
+        </div>  
+        <div class="col">
+            <input id="chk_seeds" type="checkbox" name="presets" value="2,2.02,2.52,2.98,5.5" />
+            <label for="chk_seeds"><small>The 1<sub>th</sub> Passage</small></label>
+        </div>
+    </div>
 </fieldset>
 <!-- end preset fieldset -->
 `);
