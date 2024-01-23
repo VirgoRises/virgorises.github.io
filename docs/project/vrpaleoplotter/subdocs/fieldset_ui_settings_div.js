1fb56f33c6ca59863b1427c0b7351c3f29a15639
The Scaffold of Witan <32809826+ScaffoldOfWitan@users.noreply.github.com>
2024-01-23 23:32:16 +0100

    UI reorg

--- docs/project/vrpaleoplotter/subdocs/fieldset_ui_settings_div.js
+++ docs/project/vrpaleoplotter/subdocs/fieldset_ui_settings_div.js
@@ -9,11 +9,11 @@ fieldset_ui_settings_div.insertAdjacentHTML('beforeend', `
         <input type="color" name="ui_settings" id="UI_line_color" value="#ff0000" onchange="plotMarker()" /><br/>
         
         <label id="labellinedash" for="linedash">line: n</label>
-        <input type="range" id="linedash" name="ui_settings" min="1" step="1" max="25" value="1" onchange="plotMarker()" />
+        <input type="range" id="linedash" name="ui_settings" min="1" step="1" max="25" value="1" onchange="plotMarker()" /><br/>
 
         <label id="labellinedashspc" for="linedashspc">space: n</label>
         <input type="range" id="linedashspc" name="ui_settings" min="0" step="1" max="25" value="3" onchange="plotMarker()" />
-        <br/>
+        
     </div>      
 </fieldset>
 <!-- end preset fieldset -->
